50f9f03f4da727cf13e8fb150614ce17086aa3e0
pjp0704 <junhyeong25@gmail.com>
2022-06-15 16:34:13 +0900

    [ADD] Error handler

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,17 +1,24 @@
-import { Application, NextFunction, Request, Response } from 'express';
+import { Application, NextFunction, Request, Response, json } from 'express';
 import cors from 'cors';
 import Container from 'typedi';
 import { Logger } from 'src/shared/logger';
-import { HttpException } from 'src/shared/exceptions';
+import { HttpException, NotFoundException } from 'src/shared/exceptions';
+import { routers } from 'src/routers';
 
 export const init = (app: Application) => {
   const logger = Container.get(Logger);
 
   app.use(cors());
+
   app.use((req: Request, res: Response, next: NextFunction) => {
-    next();
+    json()(req, res, next);
   });
 
+  app.use('/', routers());
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    next(new NotFoundException());
+  });
   app.use(
     (err: HttpException, req: Request, res: Response, next: NextFunction) => {
       const statusCode: number = err.statusCode || 500;
