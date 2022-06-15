9cfc0559ed4d848ed5750e87f14e0383e0452a31
pjp0704 <junhyeong25@gmail.com>
2022-06-15 16:32:48 +0900

    [ADD] Exceptions

--- src/shared/exceptions/index.ts
+++ src/shared/exceptions/index.ts
@@ -7,3 +7,27 @@ export class HttpException extends Error {
     this.statusCode = statusCode;
   }
 }
+
+export class BadRequestException extends HttpException {
+  constructor(message?: string) {
+    super(message ?? 'Bad Request', 400);
+  }
+}
+
+export class UnauthorizedException extends HttpException {
+  constructor(message?: string) {
+    super(message ?? 'Unauthorized', 401);
+  }
+}
+
+export class ForbiddenException extends HttpException {
+  constructor(message?: string) {
+    super(message ?? 'Forbidden', 403);
+  }
+}
+
+export class NotFoundException extends HttpException {
+  constructor(message?: string) {
+    super(message ?? 'Not Found', 404);
+  }
+}
