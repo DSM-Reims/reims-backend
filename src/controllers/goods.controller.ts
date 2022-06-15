cba598c3d1ea0cc38fd1b7a2019165ae23a07cc0
pjp0704 <junhyeong25@gmail.com>
2022-06-15 16:36:22 +0900

    [ADD] Controller

--- src/controllers/goods.controller.ts
+++ src/controllers/goods.controller.ts
@@ -1,11 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
 import { GoodsService } from 'src/services/goods.service';
-import { AsyncHandler } from 'src/shared/decorators/async-handler.decorator';
+import {
+  Delete,
+  Get,
+  Patch,
+  Post,
+} from 'src/shared/decorators/http-method.decorator';
+import { Prefix } from 'src/shared/decorators/prefix.decorator';
+import { ValidateApiKey } from 'src/shared/decorators/validate-key.decorator';
+import { Inject, Service } from 'typedi';
 
+@Service()
+@Prefix('/goods')
 export class GoodsController {
   constructor(private readonly service: GoodsService) {}
 
-  @AsyncHandler()
-  async getGoods() {
-    return;
-  }
+  @Post()
+  @ValidateApiKey()
+  async createGoodsApplication(req: Request, res: Response) {}
+
+  @Get()
+  @ValidateApiKey()
+  async getGoods(req: Request, res: Response) {}
+
+  @Patch('/return/:goods_uuid')
+  @ValidateApiKey('TEACHER')
+  async updateReturned(req: Request, res: Response) {}
+
+  @Delete('/:goods_uuid')
+  @ValidateApiKey()
+  async deleteApplication(req: Request, res: Response) {}
+
+  @Get('/applicated_clubs')
+  @ValidateApiKey('TEACHER')
+  async getApplicatedClubs(req: Request, res: Response) {}
 }
