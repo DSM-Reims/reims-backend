f1eaab4cf239446614148a99da8975f8aca788eb
pjp0704 <junhyeong25@gmail.com>
2022-06-16 14:01:22 +0900

    [ADD] custom type

---
+++ src/types/express.d.ts
@@ -0,0 +1,10 @@
+import * as Entity from '../shared/entities/user/user.entity';
+
+declare global {
+  namespace Express {
+    export interface User extends Entity.User {}
+    interface Request {
+      user?: User;
+    }
+  }
+}
